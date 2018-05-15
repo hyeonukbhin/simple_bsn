ac36c7f55b19fc2d48803f03b567b3590f605179
hyeonukbhin <bhu@kist.re.kr>
2018-05-16 02:04:07 +0900

    print color 수정

--- script/bsn_handler.py
+++ script/bsn_handler.py
@@ -14,21 +14,21 @@ def main():
     rospy.Subscriber("input", String, callback)
     pub = rospy.Publisher("output", String, queue_size=100)
     rate = rospy.Rate(1)
+    print("==============================START==============================")
     while not rospy.is_shutdown():
-        print("==============================START==============================")
         pre_condition = fc.check_param("/pre_condition")
         print("Pre_condition :", pre_condition)
         completion = fc.check_goal(pre_condition)
         print("Completion :", completion)
-        if completion is True:
+        if completion is False:
+            behavior = fc.do_behavior(pre_condition)
+            print("Behavior : \x1b[1;33m{}\x1b[1;m\n".format(behavior))
+            # print("\n")
+            fc.add_list(behavior)
+            fc.delete_list(behavior)
+        else:
             rospy.signal_shutdown("Goal Complete")
             print("===============================END===============================")
-
-        behavior = fc.do_behavior(pre_condition)
-        print("Behavior :", behavior)
-        fc.add_list(behavior)
-        fc.delete_list(behavior)
-
         rate.sleep()
 
 
